f3bb8b1aa5c6dc5e401df9c03577d224fdfb3866
Nam <namdeveloper.ca@gmail.com>
2024-06-20 22:11:07 +0700

    header

---
+++ src/components/header/filters.tsx
@@ -0,0 +1,13 @@
+// import { AccountFilter } from './account-filter'
+// import { DateFilter } from './date-filter'
+
+export const Filters = () => {
+  return (
+    <div className='flex flex-col items-center gap-y-2 lg:flex-row lg:gap-x-2 lg:gap-y-0'>
+      <p>AccountFilter</p>
+      <p>DateFilter</p>
+      {/* <AccountFilter />
+      <DateFilter /> */}
+    </div>
+  )
+}
