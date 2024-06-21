047e5ff7a82b3528c4758ec8cda3a0f2015ad684
Nam <namdeveloper.ca@gmail.com>
2024-06-21 18:00:06 +0700

    dizle kit

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -2,7 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 
 // for query purposes
-const queryClient = postgres(
-  'postgres://default:h3kjVmtuRXP2@ep-frosty-pond-a1t04o2c.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require'
-)
+const queryClient = postgres(process.env.DATABASE_URL!)
 export const db = drizzle(queryClient)
