a0df827eaada744cfcece1618c051da80ccade38
Nam <namdeveloper.ca@gmail.com>
2024-06-25 16:11:35 +0700

    list data to frontend

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,12 +1,22 @@
+'use client'
+
+import { useGetAccounts } from '@/features/accounts/api/use-get-accounts'
+
 // import { DataCharts } from '@/components/data-charts'
 // import { DataGrid } from '@/components/data-grid'
 
 const DashboardPage = () => {
+  const { data: accounts, isLoading } = useGetAccounts()
+
+  if (isLoading) return <div>Loading...</div>
+
   return (
     <div className='mx-auto -mt-6 w-full max-w-screen-2xl pb-10'>
-      {/* <DataGrid />
-
-      <DataCharts /> */}
+      <p>
+        {accounts?.map((account) => (
+          <div key={account.id}>{account.name}</div>
+        ))}
+      </p>
     </div>
   )
 }
