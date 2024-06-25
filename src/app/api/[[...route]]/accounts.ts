22918edb511bcb6ed19f7e57adbcb5ef3bc043e5
Nam <namdeveloper.ca@gmail.com>
2024-06-25 17:15:51 +0700

    account post: backend

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,20 +1,42 @@
 import { Hono } from 'hono'
-import { accounts } from '@/db/schema'
+import { accounts, insertAccountSchema } from '@/db/schema'
 import { db } from '@/db/drizzle'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
+import { createId } from '@paralleldrive/cuid2'
+import { zValidator } from '@hono/zod-validator'
 import { eq } from 'drizzle-orm'
 
-const app = new Hono().get('/', clerkMiddleware(), async (c) => {
-  const auth = getAuth(c)
-  if (!auth?.userId) return c.json({ error: 'Unauthorized' }, 401)
+const app = new Hono()
+  .get('/', clerkMiddleware(), async (c) => {
+    const auth = getAuth(c)
+    if (!auth?.userId) return c.json({ error: 'Unauthorized' }, 401)
 
-  console.log(auth.userId)
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId))
+    return c.json({ data })
+  })
+  .post(
+    '/',
+    clerkMiddleware(),
+    zValidator('json', insertAccountSchema.pick({ name: true })),
+    async (c) => {
+      const auth = getAuth(c)
+      if (!auth?.userId) return c.json({ error: 'Unauthorized' }, 401)
 
-  const data = await db
-    .select({ id: accounts.id, name: accounts.name })
-    .from(accounts)
-    .where(eq(accounts.userId, auth.userId))
-  return c.json({ data })
-})
+      const values = c.req.valid('json')
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          ...values,
+        })
+        .returning()
+
+      return c.json({ data })
+    }
+  )
 
 export default app
