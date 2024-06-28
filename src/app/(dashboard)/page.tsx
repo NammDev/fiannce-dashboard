ec0e4fa6bc912b1adc432377238055211d202f25
Nam <namdeveloper.ca@gmail.com>
2024-06-28 12:16:47 +0700

    submit api

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,25 +1,11 @@
-'use client'
-
-import { Button } from '@/components/ui/button'
-import { useGetAccounts } from '@/features/accounts/api/use-get-accounts'
-import { useNewAccount } from '@/features/accounts/hooks/use-new-accounts'
-
 // import { DataCharts } from '@/components/data-charts'
 // import { DataGrid } from '@/components/data-grid'
 
 const DashboardPage = () => {
-  const { onOpen } = useNewAccount()
-  const { data: accounts, isLoading } = useGetAccounts()
-  if (isLoading) return <div>Loading...</div>
-
   return (
     <div className='mx-auto -mt-6 w-full max-w-screen-2xl pb-10'>
-      <Button onClick={onOpen}>Open Sheet</Button>
-      <p>
-        {accounts?.map((account) => (
-          <div key={account.id}>{account.name}</div>
-        ))}
-      </p>
+      {/* <DataGrid />
+      <DataCharts /> */}
     </div>
   )
 }
--- src/components/header/filters.tsx
+++ src/components/header/filters.tsx
@@ -4,8 +4,6 @@
 export const Filters = () => {
   return (
     <div className='flex flex-col items-center gap-y-2 lg:flex-row lg:gap-x-2 lg:gap-y-0'>
-      <p>AccountFilter</p>
-      <p>DateFilter</p>
       {/* <AccountFilter />
       <DateFilter /> */}
     </div>
